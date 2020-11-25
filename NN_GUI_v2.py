59d41fcc811238becc688f94ad83fa89ffd03f90
Stepp Willi Leopold <willi.stepp@epfl.ch>
2020-11-25 09:55:43 +0100

    linted in vs code for pep-8

--- NN_GUI_v2.py
+++ NN_GUI_v2.py
@@ -10,8 +10,8 @@ Created on Mon Oct  5 12:18:48 2020
 @author: stepp
 """
 from PyQt5.QtCore import Qt, pyqtSignal, QT_VERSION_STR, QTimer
-from PyQt5.QtWidgets import QWidget, QSlider, QPushButton, QLabel, QGridLayout,\
-                            QFileDialog, QProgressBar, QGroupBox, QApplication
+from PyQt5.QtWidgets import QWidget, QSlider, QPushButton, QLabel,\
+    QGridLayout, QFileDialog, QProgressBar, QGroupBox, QApplication
 import pyqtgraph as pg
 from skimage import io
 from QtImageViewer import QtImageViewer
@@ -26,12 +26,11 @@ from tensorflow import keras
 QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
 
 
-
 class MultiPageTIFFViewerQt(QWidget):
 
     frameChanged = pyqtSignal([], [int])
 
-    def __init__(self,app):
+    def __init__(self, app):
         QWidget.__init__(self)
 
         # Handle to the image stack tiffcapture object.
@@ -45,18 +44,18 @@ class MultiPageTIFFViewerQt(QWidget):
         self.viewer_drp = QtImageViewer()
         self.viewer_nn = QtImageViewer()
         self.loadBox = QGroupBox()
-        
+
         # Connect events for zooming in all output data
         self.viewer_mito.rightMouseButtonReleased.connect(self.mouseRelease)
-        self.viewer_mito.rightMouseButtonDoubleClicked.connect(self.mouseDouble)
-        
+        self.viewer_mito.rightMouseButtonDoubleClicked.connect(
+            self.mouseDouble)
+
         # Slider and arrow buttons for frame traversal.
-        self.sliderBox =QGroupBox()
+        self.sliderBox = QGroupBox()
         self.frameSlider = QSlider(Qt.Horizontal)
         self.prevFrameButton = QPushButton("<")
         self.nextFrameButton = QPushButton(">")
-        
-        
+
         # loadBox content: Buttons for load model and data
         self.modelButton = QPushButton("load model")
         self.dataButton = QPushButton("load data")
@@ -66,17 +65,15 @@ class MultiPageTIFFViewerQt(QWidget):
         self.outputPlot = pg.PlotWidget()
         pen = pg.mkPen(color='#AAAAAA', style=Qt.DashLine)
         self.frameLine = pg.InfiniteLine(pos=0.5, angle=90, pen=pen)
-        self.outputPlot.addItem(self.frameLine, ignoreBounds=True)
-        
+
         # Connect functions to the interactive elements
         self.modelButton.clicked.connect(self.loadModel)
         self.dataButton.clicked.connect(self.loadData)
-        #self.prevFrameButton.clicked.connect(self.prevFrame)
-        #self.nextFrameButton.clicked.connect(self.nextFrame)
+        # self.prevFrameButton.clicked.connect(self.prevFrame)
+        # self.nextFrameButton.clicked.connect(self.nextFrame)
         self.frameSlider.sliderPressed.connect(self.startTimer)
         self.frameSlider.sliderReleased.connect(self.stopTimer)
 
-        
         # Layout.
         grid = QGridLayout(self)
         grid.addWidget(self.viewer_mitoOrig, 0, 0)
@@ -86,13 +83,13 @@ class MultiPageTIFFViewerQt(QWidget):
         grid.addWidget(self.viewer_drp, 1, 1)
         grid.addWidget(self.viewer_nn, 1, 2)
         grid.addWidget(self.sliderBox, 2, 0, 1, 3)
-        
-        gridprogress = QGridLayout(self.sliderBox)       
+
+        gridprogress = QGridLayout(self.sliderBox)
         gridprogress.addWidget(self.prevFrameButton, 0, 0)
         gridprogress.addWidget(self.frameSlider, 0, 1)
         gridprogress.addWidget(self.nextFrameButton, 0, 3)
         gridprogress.setContentsMargins(0, 0, 0, 0)
-        
+
         gridBox = QGridLayout(self.loadBox)
         gridBox.addWidget(self.currentFrameLabel, 1, 0)
         gridBox.addWidget(self.progress, 1, 1)
@@ -100,30 +97,27 @@ class MultiPageTIFFViewerQt(QWidget):
         gridBox.addWidget(self.dataButton, 0, 1)
         gridBox.addWidget(self.outputPlot, 2, 0, 1, 2)
 
-        self.resize(1500,1000)
+        self.resize(1500, 1000)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.onTimer)
         self.timer.setInterval(40)
-        
+
         self.app = app
-        
-        
+
     def loadData(self):
         # load images, this goes into a button later
         self.progress.setValue(0)
         folder = 'C:/Users/stepp/Documents/data_raw/SmartMito/'
         nnImageSize = 128
         pixelCalib = 56  # nm per pixel
-        resizeParam = pixelCalib/81 # no unit
-        
-        
-        
+        resizeParam = pixelCalib/81  # no unit
+
         mito_file = folder + 's6_c12_p0_mito.tif'
         drp_file = folder + 's6_c12_p0_drp1.tif'
         self.image_mitoOrig = io.imread(mito_file).transpose(0, 2, 1)
         self.image_drpOrig = io.imread(drp_file).transpose(0, 2, 1)
-    
+
         print(self.image_mitoOrig.shape)
         # Do NN for all images
         frameNum = self.image_mitoOrig.shape[0]
@@ -133,66 +127,57 @@ class MultiPageTIFFViewerQt(QWidget):
         drpDataFull = np.zeros_like(nnOutput)
         outputData = []
         # set up the progress bar
-        self.progress.setRange(0,frameNum-1)
-        
-             
+        self.progress.setRange(0, frameNum-1)
+
         for frame in range(0, self.image_mitoOrig.shape[0]):
-            inputData, positions = prepareNNImages(self.image_mitoOrig[frame,
-                                                                       :, :], 
-                                                self.image_drpOrig[frame, :, :], 
-                                                    nnImageSize)
-            output_predict = self.model.predict_on_batch(inputData) 
+            inputData, positions = prepareNNImages(
+                self.image_mitoOrig[frame, :, :],
+                self.image_drpOrig[frame, :, :], nnImageSize)
+            output_predict = self.model.predict_on_batch(inputData)
             i = 0
-            stitch = positions['stitch']
-            stitch1 = None if stitch == 0 else -stitch
-            for position in positions['px']:
-                nnOutput[frame, position[0]+stitch:position[2]-stitch,
-                            position[1]+stitch:position[3]-stitch] = \
-                            output_predict[i,stitch:stitch1,stitch:stitch1,0]
-                mitoDataFull[frame, position[0]+stitch:position[2]-stitch,
-                           position[1]+stitch:position[3]-stitch] = \
-                           inputData[i,stitch:stitch1,stitch:stitch1,0]
-                drpDataFull[frame, position[0]+stitch:position[2]-stitch,
-                           position[1]+stitch:position[3]-stitch] = \
-                           inputData[i,stitch:stitch1,stitch:stitch1,1]
-                            
+            st = positions['stitch']
+            st1 = None if st == 0 else -st
+            for p in positions['px']:
+                outputDataFull[p[0]+st:p[2]-st, p[1]+st:p[3]-st] =\
+                            output_predict[i, st:st1, st:st1, 0]
+                mitoDataFull[p[0]+st:p[2]-st, p[1]+st:p[3]-st] = \
+                    inputData[i, st:st1, st:st1, 0]
+                drpDataFull[p[0]+st:p[2]-st, p[1]+st:p[3]-st] = \
+                    inputData[i, st:st1, st:st1, 1]
                 i += 1
+
             outputData.append(np.max(nnOutput[frame, :, :]))
             self.progress.setValue(frame)
             self.app.processEvents()
-                    
-        self.outputPlot.plot(outputData)      
+
+        self.outputPlot.plot(outputData)
         self.image_nn = nnOutput
         self.image_mito = mitoDataFull
         self.image_drp = drpDataFull
         self.frameSlider.setMaximum(nnOutput.shape[0]-1)
         self.onTimer()
-        
-        
-        
+
         print('Done with everything')
-            
-            
+
     def loadModel(self):
-        folder = 'C:/Users/stepp/Documents/data_raw/SmartMito/'   
+        folder = 'C:/Users/stepp/Documents/data_raw/SmartMito/'
         print('Starting the model up')
         model_path = folder + 'model_Dora.h5'
         self.model = keras.models.load_model(model_path, compile=True)
         print('Model compiled')
-    
-              
+
     def onTimer(self):
         i = self.frameSlider.value()
-        qimage_mitoOrig = qimage2ndarray.array2qimage(self.image_mitoOrig
-                                                      [i, :, :],normalize=True)
-        qimage_drpOrig = qimage2ndarray.array2qimage(self.image_drpOrig
-                                                     [i, :, :], normalize=True)
-        qimage_drp = qimage2ndarray.array2qimage(self.image_drp[i, :, :],
-                                                 normalize=False)
-        qimage_mito = qimage2ndarray.array2qimage(self.image_mito[i, :, :],
-                                                  normalize=False)
-        qimage_nn = qimage2ndarray.array2qimage(self.image_nn[i, :, :],
-                                                normalize=(0, 50))
+        qimage_mitoOrig = qimage2ndarray.array2qimage(
+            self.image_mitoOrig[i, :, :], normalize=True)
+        qimage_drpOrig = qimage2ndarray.array2qimage(
+            self.image_drpOrig[i, :, :], normalize=True)
+        qimage_drp = qimage2ndarray.array2qimage(
+            self.image_drp[i, :, :], normalize=False)
+        qimage_mito = qimage2ndarray.array2qimage(
+            self.image_mito[i, :, :], normalize=False)
+        qimage_nn = qimage2ndarray.array2qimage(
+            self.image_nn[i, :, :], normalize=(0, 50))
         self.viewer_mito.setImage(qimage_mito)
         self.viewer_drp.setImage(qimage_drp)
         self.viewer_drpOrig.setImage(qimage_drpOrig)
@@ -202,20 +187,20 @@ class MultiPageTIFFViewerQt(QWidget):
         self.frameLine.setValue(i)
         self.viewer_mito.updateViewer()
 
-            
     def startTimer(self, i=0):
         self.timer.start()
-        
+
     def stopTimer(self, i=0):
         self.timer.stop()
-    
-        
+
     def mouseRelease(self, x, y):
 
         for remoteViewer in [self.viewer_drp, self.viewer_nn]:
             if remoteViewer.canZoom:
-                viewBBox = remoteViewer.zoomStack[-1] if len(remoteViewer.zoomStack) else remoteViewer.sceneRect()
-                selectionBBox = self.viewer_mito.scene.selectionArea().boundingRect().intersected(viewBBox)
+                viewBBox = remoteViewer.zoomStack[-1] if\
+                    len(remoteViewer.zoomStack) else remoteViewer.sceneRect()
+                selectionBBox = self.viewer_mito.scene.selectionArea().\
+                    boundingRect().intersected(viewBBox)
                 if selectionBBox.isValid() and (selectionBBox != viewBBox):
                     remoteViewer.zoomStack.append(selectionBBox)
                     remoteViewer.updateViewer()
@@ -226,10 +211,9 @@ class MultiPageTIFFViewerQt(QWidget):
                 remoteViewer.zoomStack = []
                 remoteViewer.updateViewer()
 
-from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
-#app.setAttribute(QtCore.Qt.AA_Use96Dpi)
+# app.setAttribute(QtCore.Qt.AA_Use96Dpi)
 stackViewer = MultiPageTIFFViewerQt(app)
 
 stackViewer.show()
